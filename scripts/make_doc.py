18bc7ab469461a1b08c558c88dd83c3ae75c07dd
Akshay <akshaybabloo@gmail.com>
2021-07-01 12:17:06 +1200

    docs added

---
+++ scripts/make_doc.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+if "scripts" in os.getcwd():
+    os.chdir("../_docs")
+else:
+    os.chdir("_docs")
+
+
+def make_doc():
+    """Run Sphinx to build the doc."""
+    try:
+        # removing previous build
+        print('removing previous build')
+        cmd = 'make clean'
+        os.system(cmd)
+        shutil.rmtree('../docs/', ignore_errors=True)
+
+        # new build
+        cmd = 'make html'
+        os.system(cmd)
+
+        # copy files - windows cmd here
+        print('copy files')
+        shutil.copytree("_build/html/", "../docs")
+    except Exception as error:
+        print(error)
+
+
+if __name__ == '__main__':
+    make_doc()
