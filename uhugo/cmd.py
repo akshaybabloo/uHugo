16b0d3338eeda8b1639cedd1707e8d064457d696
Akshay <akshaybabloo@gmail.com>
2021-07-01 12:48:19 +1200

    debug option updated

--- uhugo/cmd.py
+++ uhugo/cmd.py
@@ -1,17 +1,24 @@
+import logging
 from typing import Text
 
 import click
 
 from . import __version__
 
+log = logging.getLogger(__name__)
+
 
 @click.group(name="uhugo",
              help="uhugo is a Hugo binary helper that downloads and set ups the environment.")
-@click.option('--debug', help="Use debug mode", is_flag=True)
+@click.option('--debug/--no-debug', help="Use debug mode", default=False)
 @click.version_option(__version__, package_name="uHugo", prog_name="uHugo")
 @click.pass_context
 def cli(ctx: click.core.Context, debug: bool):
+    ctx.ensure_object(dict)
     ctx.obj['debug'] = debug
+
+    if debug:
+        logging.basicConfig(level='DEBUG')
 
 
 @cli.command(help="Install latest Hugo binary files")
