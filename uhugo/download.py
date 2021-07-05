df7446822eb9ac4939f7496dd480bebf66d976eb
Akshay <akshaybabloo@gmail.com>
2021-07-06 11:32:02 +1200

    use rich for progress bar

--- uhugo/download.py
+++ uhugo/download.py
@@ -3,7 +3,21 @@ import platform
 import tempfile
 
 import requests
-from tqdm import tqdm
+from rich.progress import Progress, TextColumn, BarColumn, DownloadColumn, TransferSpeedColumn, \
+    TimeRemainingColumn
+
+progress = Progress(
+    TextColumn("[bold blue]{task.fields[filename]}", justify="right"),
+    BarColumn(bar_width=None),
+    "[progress.percentage]{task.percentage:>3.1f}%",
+    "•",
+    DownloadColumn(),
+    "•",
+    TransferSpeedColumn(),
+    "•",
+    TimeRemainingColumn(),
+    transient=True
+)
 
 
 def download_hugo_zip(version: str, os_type: str = platform.system(), download_to: str = tempfile.gettempdir()) -> str:
@@ -16,35 +30,42 @@ def download_hugo_zip(version: str, os_type: str = platform.system(), download_t
     """
 
     download_to = os.path.join(download_to, f"hugo_{version}")
+    with progress:
+        task_id = progress.add_task("download", filename=f"hugo_{version}", start=False)
 
-    with open(download_to, "wb") as file:
-        if os_type == 'Darwin':
-            response = requests.get(
-                f"https://github.com/gohugoio/hugo/releases/download/v{version}/hugo_extended_{version}_macOS-64bit.tar.gz",
-                stream=True)
-        elif os_type == 'Windows' or os_type == 'nt':
-            response = requests.get(
-                f"https://github.com/gohugoio/hugo/releases/download/v{version}/hugo_extended_{version}_Windows-64bit.zip",
-                stream=True)
-        elif os_type == "posix" or os_type == "Linux":
-            response = requests.get(
-                f"https://github.com/gohugoio/hugo/releases/download/v{version}/hugo_extended_{version}_Linux-64bit.zip",
-                stream=True)
-        else:
-            raise OSError(f"{os_type} not supported.")
+        with open(download_to, "wb") as file:
+            if os_type == 'Darwin':
+                response = requests.get(
+                    f"https://github.com/gohugoio/hugo/releases/download/v{version}/hugo_extended_{version}_macOS-64bit.tar.gz",
+                    stream=True)
+            elif os_type == 'Windows' or os_type == 'nt':
+                response = requests.get(
+                    f"https://github.com/gohugoio/hugo/releases/download/v{version}/hugo_extended_{version}_Windows-64bit.zip",
+                    stream=True)
+            elif os_type == "posix" or os_type == "Linux":
+                response = requests.get(
+                    f"https://github.com/gohugoio/hugo/releases/download/v{version}/hugo_extended_{version}_Linux-64bit.zip",
+                    stream=True)
+            else:
+                raise OSError(f"{os_type} not supported.")
 
-        if response.headers.get('Status') == "404 Not Found":
-            raise requests.exceptions.HTTPError("File not found")
+            if response.headers.get('Status') == "404 Not Found":
+                raise requests.exceptions.HTTPError("File not found")
 
-        print(f"Downloading Hugo v{version} to: ", download_to)
-        total_length = int(response.headers.get('content-length'))
+            total_length = int(response.headers.get('content-length'))
 
-        if total_length is None:
-            file.write(response.content)
-        else:
-            with tqdm(total=total_length, unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+            if total_length is None:
+                progress.console.print(f"- Downloading Hugo v{version}")
+                file.write(response.content)
+                progress.update(task_id, completed=True)
+                progress.console.print("- Hugo downloaded")
+            else:
+                progress.console.print(f"- Downloading Hugo v{version}")
+                progress.update(task_id, total=total_length)
+                progress.start_task(task_id)
                 for data in response.iter_content(chunk_size=4096):
                     file.write(data)
-                    pbar.update(len(data))
+                    progress.update(task_id, advance=len(data))
+                progress.console.print("- Hugo downloaded")
 
     return download_to
