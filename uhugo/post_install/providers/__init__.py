8d18be3e77f7a947ed32f6f1b932246ca9a035aa
Akshay Raj Gollahalli <akshay@gollahalli.com>
2021-07-09 13:55:40 +1200

    api_key and config_path params added

--- uhugo/post_install/providers/__init__.py
+++ uhugo/post_install/providers/__init__.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+import os
 from typing import Text
 
 
@@ -7,18 +7,35 @@ class ProviderBase:
     This is a base class for all the providers
     """
 
-    def update_api(self, key: Text):
+    def __init__(self, api_key: Text = None, config_path: Text = None):
+        """
+
+        :param api_key: Authentication key for providers
+        :param config_path: Path for configuration file of the provider
+        """
+        self.api_key = api_key
+        self.path = config_path
+
+        if not self.api_key:
+            self.api_key = os.environ.get("UHUGO_KEY", None)
+
+        if not self.path:
+            self.path = os.environ.get("UHUGO_CONFIG_PATH", None)
+
+        if not self.api_key and not self.path:
+            raise ValueError("key or path not set")
+
+    def update_api(self, project_name: Text = None):
         """
         Updates an API
         :return:
         """
         pass
 
-    def update_file(self, path: Path):
+    def update_file(self):
         """
         Updates a configuration file
 
-        :param path: Path to a configuration file
         :return:
         """
         pass
